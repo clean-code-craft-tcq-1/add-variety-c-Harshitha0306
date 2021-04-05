9f702c1436ccacd57c843f05dcb0de1e2fec17fb
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-04-06 01:10:14 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -1,9 +1,9 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "catch.hpp"
-extern "C" {
+#include "test/catch.hpp"
+
 #include "typewise-alert.h"
-}
+
 
 TEST_CASE("infers the breach according to temperature lower than limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
