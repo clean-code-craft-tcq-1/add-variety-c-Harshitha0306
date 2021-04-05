69ccc1bb24a7196c1a92169cb8e171161801f8d5
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-04-06 01:08:49 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -1,21 +1,31 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "test/catch.hpp"
+#include "catch.hpp"
+extern "C" {
 #include "typewise-alert.h"
+}
 
-TEST_CASE("infers the breach according to lower limits") {
+TEST_CASE("infers the breach according to temperature lower than limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
 
-TEST_CASE("infers the breach according to higher limits") {
+TEST_CASE("infers the breach according to temperature higher than limits") {
   REQUIRE(inferBreach(32, 20, 30) == TOO_HIGH);
 }
 
-TEST_CASE("Temperature within limits") {
+TEST_CASE("No breach,Temperature within limits") {
   REQUIRE(inferBreach(22, 20, 30) == NORMAL);
 }
 
-
-TEST_CASE("Checks Temperarture breach based on cooling type") {
+TEST_CASE("Checks Temperarture breach based on Passive cooling type") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,40) == TOO_HIGH);
 }
+
+TEST_CASE("Checks Temperarture breach based on HI-Active cooling type") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,-1) == TOO_LOW);
+}
+
+TEST_CASE("Checks Temperarture breach based on Med-Active cooling type") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,20) == NORMAL);
+}
+
