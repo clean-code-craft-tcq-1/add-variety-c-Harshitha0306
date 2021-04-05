0567aadbdcb155ff41b9b3333ef1378886106729
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-04-06 01:07:59 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -21,9 +21,6 @@
 
 CoolingTypeLimits Limits[] ={{PASSIVE_COOLING,0,35},{HI_ACTIVE_COOLING,0,45},{MED_ACTIVE_COOLING,0,40}};
 
-void (*display_arr[])(const char*) ={Display_ForNormal,Display_FortooHigh,Display_FortooLow};
-void (*target_arr[])(BreachType) ={sendToController,sendToEmail};
-
 
 /*This Function checks whether the input Temperature has breached lower limit or Upper limit value */
 
@@ -44,52 +41,5 @@ BreachType classifyTemperatureBreach(
  return inferBreach(temperatureInC, Limits[coolingType].lowerLimit,Limits[coolingType].upperLimit);
 }
 
-/*This Function sends alert message to Different targets when there is temperature breach*/
 
-void checkAndAlert(
-    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
 
-  BreachType breachType = classifyTemperatureBreach(
-    batteryChar.coolingType, temperatureInC
-  );
-    (*target_arr[alertTarget])(breachType);
-  
-}
-/*This Function sends alert message to Controller when there is temperature breach based on Cooling type*/
-
-void sendToController(BreachType breachType) {
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
-}
-
-/*This Function displays that temperature is Normal*/
-
-void Display_ForNormal(const char* recepient)
-{
-    printf("To: %s\n", recepient);
-    printf("Hi, the temperature is normal\n");
-}
-
-/*This Function displays that temperature is too high*/
-
-void Display_FortooHigh(const char* recepient)
-{
-    printf("To: %s\n", recepient);
-    printf("Hi, the temperature is too high\n");
-}
-
-/*This Function displays that temperature is too low*/
-
-void Display_FortooLow(const char* recepient)
-{
-    printf("To: %s\n", recepient);
-    printf("Hi, the temperature is too low\n");
-}
-
-/*This Function sends alert message to Email when there is temperature breach based on Cooling type*/
-
-void sendToEmail(BreachType breachType) {
-  const char* recepient = "a.b@c.com";
-    (*display_arr[breachType])(recepient);
-  
-}
