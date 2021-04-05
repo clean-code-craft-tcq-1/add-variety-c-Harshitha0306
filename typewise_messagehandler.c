9168dac9d3bf6861786d339a3afc6221262ad56d
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-04-06 02:55:20 +0530

    Update typewise_messagehandler.c

--- typewise_messagehandler.c
+++ typewise_messagehandler.c
@@ -13,60 +13,76 @@
 #include "typewise-alert.h"
 #include "typewise_messagehandler.h"
 
+/*
+***************************************************************************************
+* Declarations
+***************************************************************************************
+*/
+
+Received_MessageTarget (*display_arr[])(const char*) ={DisplayMessage_ForNormal,DisplayMessage_FortooLow,DisplayMessage_FortooHigh};
+
+Received_MessageTarget (*target_arr[])(BreachType) = {sendToController,sendToEmail,sendToConsole};
 
 /*This Function sends alert message to Different targets when there is temperature breach*/
 
-void checkAndAlert(
+Received_MessageTarget checkAndAlert(
     AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
 
   BreachType breachType = classifyTemperatureBreach(
     batteryChar.coolingType, temperatureInC
   );
-    (*target_arr[alertTarget])(breachType);
+   return( (*target_arr[alertTarget])(breachType));
   
 }
 
 /*This Function sends alert message to Contro
  ller when there is temperature breach based on Cooling type*/
 
-void sendToController(BreachType breachType) {
+Received_MessageTarget sendToController(BreachType breachType) {
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
+  return StatusToController;
 }
 
 /*This Function displays that temperature is Normal*/
 
-void DisplayMessage_ForNormal(const char* recepient)
+Received_MessageTarget DisplayMessage_ForNormal(const char* recepient)
 {
     printf("To: %s\n", recepient);
     printf("Hi, the temperature is normal\n");
+    return StatusToEmail_Normal;
 }
 
 /*This Function displays that temperature is too high*/
 
-void DisplayMessage_FortooHigh(const char* recepient)
+Received_MessageTarget DisplayMessage_FortooHigh(const char* recepient)
 {
     printf("To: %s\n", recepient);
     printf("Hi, the temperature is too high\n");
+    return StatusToEmail_TooHigh;
 }
 
 /*This Function displays that temperature is too high*/
 
-void DisplayMessage_FortooLow(const char* recepient)
+Received_MessageTarget DisplayMessage_FortooLow(const char* recepient)
 {
     printf("To: %s\n", recepient);
     printf("Hi, the temperature is too low\n");
+    return StatusToEmail_TooLow;
 }
 
 /*This Function sends alert message to Email when there is temperature breach based on Cooling type*/
 
-void sendToEmail(BreachType breachType) {
+Received_MessageTarget sendToEmail(BreachType breachType) {
   const char* recepient = "a.b@c.com";
-    (*display_arr[breachType])(recepient);
+   return( (*display_arr[breachType])(recepient));
   
 }
 /*This Function sends alert message to Console when there is temperature breach based on Cooling type*/
 
-void sendToConsole(BreachType breachType) {
+Received_MessageTarget sendToConsole(BreachType breachType) {
   printf("The Temperature is %x\n", breachType);
+  return StatusToConsole;
 }
+
+
